2b7259f2eeced3097efc0475f79a833f60741821
Faisal Ahammed <50483833+faisalahammed044274@users.noreply.github.com>
2020-11-15 00:18:42 +0600

    COMPOSER TEST
    Here i am testing composer.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require ('./vendor/autoload.php');
+
+
+displaymodel1();
+displaymodel2();
+displaymodel3();
+$realme = new Realmemobile;
+$redmi = new Redmimobile;
+$c1 = new Class1;
+$c2 = new Class2;
+?>
